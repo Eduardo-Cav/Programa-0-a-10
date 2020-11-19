9094a8147b2840844f7bbb3462fb91210fba4d70
Eduardo <eduzika2903@hotmail.com>
2020-11-19 19:43:37 -0300

    Progrma de nota realizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Programa_0_a_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            float nota;
+            do{
+                Console.WriteLine("Digite sua nota: ");
+                nota = float.Parse(Console.ReadLine());
+
+                if(nota < 0 || nota > 10){
+                    Console.WriteLine("Esse número não é válido");
+                }
+            }while(nota < 0 || nota > 10);
+            
+            Console.WriteLine("Número Aceito");
+        }
+    }
+}
